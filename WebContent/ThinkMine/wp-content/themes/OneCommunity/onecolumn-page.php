ead2a77c8f3cccf707f262b27b1c0f3cae15c185
jdj9354 <jdj9354@gmail.com>
2015-05-22 00:24:35 +0900

    Added New mind map button

--- WebContent/ThinkMine/wp-content/themes/OneCommunity/onecolumn-page.php
+++ WebContent/ThinkMine/wp-content/themes/OneCommunity/onecolumn-page.php
@@ -6,6 +6,60 @@ Template Name: Full Width
 
 <script src="/ThinkMineCV/js/ThinkMineLib.js"></script>
 <script src="/ThinkMineCV/js/ThinkMineUILib.js"></script>
+
+<style type='text/css'>
+.maxbutton-1.maxbutton {
+  position : relative;
+  text-decoration : none;
+  display : inline-block;
+  cursor : default;
+  width : 158px;
+  border-color : #eb0316;
+  border-top-left-radius : 4px;
+  border-top-right-radius : 4px;
+  border-bottom-left-radius : 4px;
+  border-bottom-right-radius : 4px;
+  border-style : double;
+  border-width : 1px;
+  background: #f70000;
+  -pie-background: linear-gradient(#f70000 45%, #b01515);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(45%, #f70000), color-stop(1, #b01515));
+  background: -moz-linear-gradient(#f70000 45%, #b01515);
+  background: -o-linear-gradient(#f70000 45%, #b01515);
+  background: linear-gradient(#f70000 45%, #b01515);
+  -webkit-box-shadow: 0px 0px 2px #333;
+  -moz-box-shadow: 0px 0px 2px #333;
+  box-shadow: 0px 0px 2px #333; }
+
+.maxbutton-1.maxbutton:hover {
+  border-color : #eb0316;
+  background: #b01515;
+  -pie-background: linear-gradient(#b01515 45%, #f70000);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(45%, #b01515), color-stop(1, #f70000));
+  background: -moz-linear-gradient(#b01515 45%, #f70000);
+  background: -o-linear-gradient(#b01515 45%, #f70000);
+  background: linear-gradient(#b01515 45%, #f70000);
+  -webkit-box-shadow: 0px 0px 2px #333;
+  -moz-box-shadow: 0px 0px 2px #333;
+  box-shadow: 0px 0px 2px #333; }
+
+.maxbutton-1.maxbutton .mb-text {
+  font-family : Verdana;
+  font-size : 16px;
+  font-style : normal;
+  font-weight : normal;
+  padding-top : 15px;
+  padding-right : 25px;
+  padding-bottom : 15px;
+  padding-left : 25px;
+  line-height : 1em;
+  box-sizing : border-box;
+  display : block;
+  color : #fff; }
+
+.maxbutton-1.maxbutton:hover .mb-text {
+  color : #fff; }</style>
+
 <?php get_header(); ?>
 
 	<div id="content">
@@ -15,6 +69,7 @@ Template Name: Full Width
 		<?php do_action( 'bp_before_blog_page' ); ?>
 
 		<div class="page" id="blog-page" role="main">
+			<a  class="maxbutton-1 maxbutton" href="javascript:void(0);"><span class='mb-text'>Add MindMap</span></a>
 			<canvas id="tmCanvas" height = "800" width="1500"></canvas>	
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
